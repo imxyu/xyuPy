5624b06fc432c649fe0a4fa33999f9db85e37b1b
xyu <tellxysth@gmail.com>
2019-05-20 12:06:53 +0800

    interactive mode

--- montage.py
+++ montage.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt 
 from skimage.util import montage as m
 
-def montage(input):
-    output = montage(input)
-    if len(output.shape) == 3:
-        plt.imshow(output, cmap='gray')
+def montage(input_arr):
+    output_arr = m(input_arr)
+    plt.ion()
+    plt.figure()
+    if len(input_arr.shape) == 3:
+        plt.imshow(output_arr, cmap='gray')
         plt.show()
     else:
-        plt.imshow(output)
+        plt.imshow(output_arr)
         plt.show()
